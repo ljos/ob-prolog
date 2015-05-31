238651c028b2d456a90fd5d2ed81588fabeee4c6
Bjarte Johansen <bjarte.johansen@gmail.com>
2015-05-31 15:58:54 +0200

    Test by name and better session tests

--- test/test-ob-prolog.el
+++ test/test-ob-prolog.el
@@ -27,53 +27,77 @@
 (unless (featurep 'ob-prolog)
   (signal 'missing-test-dependency "Support for Prolog code blocks"))
 
-(defmacro test-ob-prolog/test-src-block (number &rest body)
+(defmacro test-ob-prolog/test-src-block (name &rest body)
   (declare (indent 1))
   (let ((buf (make-symbol "buf"))
 	(visited-p (make-symbol "visited-p"))
 	(file-pos (make-symbol "file-pos"))
+	(active-session (make-symbol "active-session"))
 	(file "test-ob-prolog.org"))
-    `(let ((,visited-p (get-file-buffer ,file))
-	   (,buf (find-file-noselect ,file)))
-       (unwind-protect
-	   (with-current-buffer ,buf
-	     (goto-char (point-min))
-	     (condition-case nil
-		 (progn
-		   (org-show-subtree)
-		   (org-show-block-all))
-	       (error nil))
-	     (dotimes (,(make-symbol "_") ,number)
-	       (org-babel-next-src-block))
-	     (save-restriction ,@body))
-	 (unless ,visited-p
-	   (kill-buffer ,buf))))))
+    `(let* ((,visited-p (get-file-buffer ,file))
+	    (,buf (or ,visited-p
+		      (find-file-noselect ,file)))
+	    ,active-session)
+       (with-current-buffer ,buf
+	 (unwind-protect
+	     (save-match-data
+	       (save-excursion
+		 (goto-char (point-min))
+		 (condition-case nil
+		     (progn
+		       (org-show-subtree)
+		       (org-show-block-all))
+		   (error nil))
+		 (org-babel-goto-named-src-block ,(symbol-name name))
+		 (save-excursion
+		   (ignore-errors
+		     (org-babel-previous-src-block)
+		     (end-of-line)
+		     (let* ((info (nth 2 (org-babel-get-src-block-info)))
+			    (session (cdr (assq :session info)))
+			    (bound (point)))
+		       (setq ,active-session
+			     (unless (string= "none" session)
+			       session))
+		       (goto-char (point-min))
+		       (while (search-forward (concat ":session " session) bound t)
+			 (org-babel-execute-src-block)))))
+		 (save-restriction ,@body)))
+	   (unless ,visited-p
+	     (kill-buffer ,buf))
+	   (when ,active-session
+	     (kill-buffer ,active-session)))))))
 (def-edebug-spec test-ob-prolog/test-src-block (form body))
 
 (ert-deftest test-ob-prolog/simple-execution ()
   "Test simple execution of prolog source block."
-  (test-ob-prolog/test-src-block 1
+  (test-ob-prolog/test-src-block basic-test
     (should (string= "Hello, org_mode."
 		     (org-babel-execute-src-block)))))
 
 (ert-deftest test-ob-prolog/goal-execution ()
   "Test execution of goal argument to prolog source block."
-  (test-ob-prolog/test-src-block 2
+  (test-ob-prolog/test-src-block goal-test
     (should (string= "Hello, world!"
 		     (org-babel-execute-src-block)))))
 
 (ert-deftest test-ob-prolog/simple-running-session ()
-  (test-ob-prolog/test-src-block 3
+  "Test running a session."
+  (test-ob-prolog/test-src-block session-test
     (should (string= "A = 41."
 		     (org-babel-execute-src-block)))))
 
-(ert-deftest test-ob-prolog/simple-running-session ()
-  (test-ob-prolog/test-src-block 4
+(ert-deftest test-ob-prolog/call-predicate-in-session ()
+  "Test calling a predicate in the session that is defined in
+another block."
+  (test-ob-prolog/test-src-block other-predicate-test
     (should (string= "A = 42."
 		     (org-babel-execute-src-block)))))
 
-(ert-deftest test-ob-prolog/simple-running-session ()
-  (test-ob-prolog/test-src-block 6
+(ert-deftest test-ob-prolog/interacting-with-other-block ()
+  "Test interacting with source block that is not a prolog
+block."
+  (test-ob-prolog/test-src-block interaction-test
     (should (string= "A = [0, 1, 2, 3]."
 		     (org-babel-execute-src-block)))))
 
